f9ce098332b388b756d6e605816b1b1f65bb9cd0
nicolasmanurung <nicolas.manurung@dana.id>
2022-12-07 19:37:31 +0700

    Init Buyer Data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
+    private Integer sumPeople;
+
+    private String nameBuyer;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    private void setSumPeople(Integer sumPeople) {
+        this.sumPeople = sumPeople;
+    }
+
+    private void setNameBuyer(String nameBuyer) {
+        this.nameBuyer = nameBuyer;
+    }
+
 }
