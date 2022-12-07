1eb8b582fb6d8c329b691675c8b92b56a8808674
nicolasmanurung <nicolas.manurung@dana.id>
2022-12-07 19:49:17 +0700

    Create TODO List

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.List;
+
 public class Main {
 
     private Integer sumPeople;
 
     private String nameBuyer;
+
+    private final Integer MAX_ORDER = 10;
+
+    //TODO("Create List Menu Variabel - Use List into this and @MenuSpecial.java")
+
+    //TODO("Create List Menu Order Here")
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -16,5 +24,13 @@ public class Main {
     private void setNameBuyer(String nameBuyer) {
         this.nameBuyer = nameBuyer;
     }
+
+    //TODO("Initialize list default menu here")
+
+    //TODO("Create function to list Menu Order here, we can using looping like 'for' to input order menu")
+
+    //TODO("Create function to counting menu * qty")
+
+    //TODO("showing final output")
 
 }
