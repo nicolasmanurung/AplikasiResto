5173bc92d9319fef0340da7e1c626e8cb2215fdd
nicolasmanurung <nicolas.manurung@dana.id>
2022-12-11 16:44:06 +0700

    Final push for tasks

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,142 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    private Integer sumPeople;
+    private int sumPeople;
 
     private String nameBuyer;
 
-    private final Integer MAX_ORDER = 10;
+    private final int MAX_ORDER = 10;
 
-    //TODO("Create List Menu Variabel - Use List into this and @MenuSpecial.java")
+    private final int DISCOUNT = 10;
 
-    //TODO("Create List Menu Order Here")
+    private List<MenuSpecial> listMenuSpecial;
+
+    private List<OrderItem> listOrderItems;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main myObject = new Main();
+        Scanner myInput = new Scanner(System.in);
+
+        System.out.println("Selamat siang...");
+
+        System.out.println("Pesan untuk berapa orang : ");
+        int sumPeopleInput = myInput.nextInt();
+        myObject.setSumPeople(sumPeopleInput);
+
+        System.out.println("Pesanan atas nama : ");
+        String nameBuyerInput = myInput.next();
+        myObject.setNameBuyer(nameBuyerInput);
+
+        myObject.listMenuSpecial = myObject.setDefaultMenu();
+        myObject.showDefaultMenu(myObject.listMenuSpecial);
+        System.out.println("\n\n\n");
+
+        myObject.setListOrderItems(myObject.askingBuyerHowMuchTheyOrder(myObject.listMenuSpecial));
+        myObject.showingListOrder(myObject.getListOrderItems());
     }
 
 
-    private void setSumPeople(Integer sumPeople) {
+    private void setSumPeople(int sumPeople) {
         this.sumPeople = sumPeople;
+    }
+
+    private int getSumPeople(){
+        return this.sumPeople;
     }
 
     private void setNameBuyer(String nameBuyer) {
         this.nameBuyer = nameBuyer;
     }
 
-    //TODO("Initialize list default menu here")
+    private void setListOrderItems(List<OrderItem> listOrderItems) {
+        this.listOrderItems = listOrderItems;
+    }
 
-    //TODO("Create function to list Menu Order here, we can using looping like 'for' to input order menu")
+    private List<OrderItem> getListOrderItems() {
+        return this.listOrderItems;
+    }
 
-    //TODO("Create function to counting menu * qty")
 
-    //TODO("showing final output")
+    private List<MenuSpecial> setDefaultMenu() {
+        List<MenuSpecial> listMenu = new ArrayList<>();
+        listMenu.add(new MenuSpecial("Nasi Goreng Special ", 10000.50f));
+        listMenu.add(new MenuSpecial("Nasi Goreng a ", 15000.20f));
+        listMenu.add(new MenuSpecial("Nasi Goreng b ", 6000.00f));
+        listMenu.add(new MenuSpecial("Nasi Goreng c ", 8000.15f));
+        return listMenu;
+    }
 
+    private void showDefaultMenu(List<MenuSpecial> listMenu) {
+        System.out.println("Menu Special hari ini\n==============");
+        for (int i = 0; i < listMenu.size(); i++) {
+            String menuItem = String.format("\t\t %d. %s \t\t @ Rp. %.2f", i + 1, listMenu.get(i).nameMenu, listMenu.get(i).priceMenu);
+            System.out.println(menuItem);
+        }
+    }
+
+    private List<OrderItem> askingBuyerHowMuchTheyOrder(List<MenuSpecial> listMenu) {
+        Scanner myInput = new Scanner(System.in);
+        List<OrderItem> listOrder = new ArrayList<>();
+
+        for (int i = 0; i < listMenu.size(); i++) {
+            String menuItem = String.format("\t\t %d. %s \t\t @ Rp. %.2f", i + 1, listMenu.get(i).nameMenu, listMenu.get(i).priceMenu);
+            System.out.println(menuItem);
+            int qtyOrder = myInput.nextInt();
+
+            if (qtyOrder < 0) {
+                listOrder.add(new OrderItem(listMenu.get(i).nameMenu, listMenu.get(i).priceMenu, 0));
+            } else if (qtyOrder > MAX_ORDER) {
+                listOrder.add(new OrderItem(listMenu.get(i).nameMenu, listMenu.get(i).priceMenu, 10));
+            } else {
+                listOrder.add(new OrderItem(listMenu.get(i).nameMenu, listMenu.get(i).priceMenu, qtyOrder));
+            }
+        }
+
+        return listOrder;
+    }
+
+    private float sumOrder(int qty, float price) {
+        return qty * price;
+    }
+
+    private void showingListOrder(List<OrderItem> listOrder) {
+        float sumOrder = 0;
+        float totalDiscount = 0;
+
+        for (int i = 0; i < listOrder.size(); i++) {
+            String orderItem = String.format("\t\t %d. %s \t\t %d * @ Rp. %.2f = Rp. %.2f",
+                    i + 1, listOrder.get(i).nameMenu,
+                    listOrder.get(i).qty,
+                    listOrder.get(i).priceMenu,
+                    sumOrder(listOrder.get(i).qty, listOrder.get(i).priceMenu));
+
+            if (i == listOrder.size() - 1) {
+                System.out.println(orderItem + " +");
+                System.out.println("===================================================");
+            } else {
+                System.out.println(orderItem);
+            }
+            sumOrder = sumOrder + sumOrder(listOrder.get(i).qty, listOrder.get(i).priceMenu);
+        }
+
+        String totalPurchase = String.format("Total Pembelian \t\t\t\t\t = Rp. %.2f", sumOrder);
+        System.out.println(totalPurchase);
+
+        totalDiscount = (DISCOUNT * sumOrder) / 100;
+        String discount = String.format("\nDisc. %d %% <Masa Promosi> \t\t\t\t\t = Rp. %.2f -", DISCOUNT, totalDiscount);
+        System.out.println(discount);
+
+        System.out.println("===================================================");
+
+        float finalPricePerPeople = sumOrder - ((DISCOUNT * sumOrder) / 100);
+        String totalPurchaseAfterDiscount = String.format("\nTotal Pembelian setelah discount %d %% \t\t\t\t\t = Rp. %.2f", DISCOUNT, finalPricePerPeople);
+        System.out.println(totalPurchaseAfterDiscount);
+
+        float finalPriceForAll = finalPricePerPeople / getSumPeople();
+        String totalPurchaseForAll = String.format("\nPembelian per orang <untuk %d orang> \t\t\t\t\t = Rp. %.2f", getSumPeople(), finalPriceForAll);
+        System.out.println(totalPurchaseForAll);
+    }
 }
--- src/OrderItem.java
+++ src/OrderItem.java
@@ -1,9 +1,12 @@
-public class MenuSpecial {
+public class OrderItem {
     String nameMenu = "";
     float priceMenu = 0;
 
-    MenuSpecial(String nameMenu, float priceMenu){
+    int qty = 0;
+
+    OrderItem(String nameMenu, float priceMenu, int qty){
         this.nameMenu = nameMenu;
         this.priceMenu = priceMenu;
+        this.qty = qty;
     }
 }
